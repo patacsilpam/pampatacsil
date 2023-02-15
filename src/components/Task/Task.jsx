6967d2cc342328cab65c56637d2b7317d8cc739c
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-02-15 13:29:49 +0800

    Update Task.jsx

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -14,7 +14,7 @@ export const Task = () => {
           {projects.map(project => {
             return (
               <div key={project.id}>
-                  <img  src={svg} />
+                  <img  src={project.img} />
                   <div className="img-overlay">
                     <div>
                       <a href={project.live_url} target={'_blank'}><FontAwesomeIcon icon={faArrowUpRightFromSquare}  /></a>
