f80802268e69a0448f640f400c7374d762dd652b
Pam Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-26 10:54:09 +0800

    edit resume

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,17 @@
 import React,{useRef,useEffect} from 'react'
-import resume from '../../assets/pdf/resume.pdf'
-//import gsap from 'gsap'
-/** const helloRef = useRef(null);
-  useEffect(()=>{
-    const elHello = helloRef.current;
-    gsap.fromTo(elHello,{rotation:0},{rotation:-20,duration:3})
-  },[]) */
+import resume from '../../assets/pdf/PATACSIL_PAMELA_RESUME.pdf'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 export const Home = () => { 
  
   return (
       <section className='home' id='home'>
           <div>
-            <span >&#128075; Hello,</span>
-            <h1>I'm <span>Pam Patacsil</span></h1>
+            <span >&#128075; Hello, I'm</span>
+            <h1><span>Pam Patacsil</span></h1>
             <p>I focus on creating web & mobile applications based in  the Philippines.</p>
             <div><br />
-              <a href={resume} className='btn-download-cv' target="_blank">DOWNLOAD CV</a>
+              <a href={resume} className='btn-download-cv' target="_blank">Resume</a>
             </div>
           </div>
       </section>
