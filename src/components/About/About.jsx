0670ffe4a31c3d843a21b1fde10636f444abc5a6
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-07-17 18:37:40 +0800

    Update About.jsx
    Correct grammar :D

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,9 +8,9 @@ export const About = () => {
           <p>
             Hi! My name is Pamela. You can call me Pam. I graduated with an IT degree from Urdaneta City University. 
             Throughout college, I immersed myself in various projects that allowed me to hone my skills in developing software. 
-            I successfully developed static websites, Android mobile applications, and full-stack web applications. <br /><br />
+            I successfully developed static websites, Android mobile applications, and full-stack web applications.
              <br /><br />
-            I am highly interested in pursuing software development.  
+            Currently, I am highly interested in pursuing software development.  
             I have a huge passion for building software, as it allows me to combine my technical skills with my problem-solving abilities. 
             I find personal satisfaction in turning ideas into functional and user-friendly software solutions. 
           </p>
