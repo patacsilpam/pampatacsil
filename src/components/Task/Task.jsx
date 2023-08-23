67a257c947a990b7b890c1301bd8dbf7892c81e3
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-08-23 23:24:46 +0800

    projects: updated projects section design

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -14,15 +14,19 @@ export const Task = () => {
           {projects.map(project => {
             return (
               <div key={project.id}>
-                  <img  src={project.img} />
+                  <img  src={project.img} alt={project.title}/>
                   <div className="img-overlay">
-                    <div>
-                      <a href={project.live_url} target={'_blank'}><FontAwesomeIcon icon={faArrowUpRightFromSquare}  /></a>
-                      <a href={project.github_link} target={'_blank'}><FontAwesomeIcon icon={faGithub} /></a>
-                    </div>
-                    <h1>{project.title}</h1>
-                    <p>{project.description}</p>
-                    <small>{project.tools}</small>
+                      <div>
+                        <a href={project.live_url} target={'_blank'}><FontAwesomeIcon icon={faArrowUpRightFromSquare}  /></a>
+                        <a href={project.github_link} target={'_blank'}><FontAwesomeIcon icon={faGithub} /></a>
+                      </div>
+                      <div>
+                        <h1>{project.title}</h1>
+                        <p>{project.description}</p>
+                      </div>
+                     <div>
+                        <small>{project.tools}</small>
+                     </div>
                   </div>
               </div>
             )
