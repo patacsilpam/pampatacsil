9ae07f39313326b8e5bb2601f9f229361024dfb5
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-23 21:31:14 +0800

    feat: enhanced content

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className="relative min-h-[600px] h-full flex flex-col justify-center items-center space-y-5  z-20"
+      className="relative min-h-[450px] h-full flex flex-col justify-center items-center space-y-5  z-20"
     >
       {/*
       <img
@@ -14,10 +14,11 @@ const Contact = () => {
       /> */}
       <div className="z-20 grid place-items-center gap-y-2">
         <p className="md:text-5xl text-2xl max-w-4xl text-center font-light">
-          Let's work on something that leads to remarkable success.
+          {/*Let's work on something that leads to remarkable success. */}
+          Let's work something great together
         </p>
         <p className="md:text-lg sm:text-md font-light shadow-inherit px-7">
-          Together, we'll turn your vision into a thriving reality and drive
+          Together, we'll turn your vision into a reality and drive
           impactful results.
         </p>
         <a
