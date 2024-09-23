32f6edcafac0f93173f7753ab40d604efdb608ea
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-23 21:30:10 +0800

    feat: enhanced content

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const TechStack = () => {
   return (
     <div className='flex flex-col justify-center items-center min-h-[768px] h-full'>
-      <h1 className='md:text-5xl text-2xl px-2 font-semibold font-openSans md:my-16 my-5 text-center'>Tech Stack that I've been using</h1>
+      <h1 className='md:text-5xl text-2xl px-2 font-semibold font-openSans md:my-16 my-5 text-center'>My Tech Stack</h1>
       <div className='grid  md:grid-cols-4 grid-cols-2 grid-rows-4 place-items-center '>
           {techList.map((item,key) => (
             <div key={key} className='border border-[#edededf4] max-h-3xl h-full w-[200px] flex justify-center items-center'>
