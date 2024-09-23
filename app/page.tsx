20c5feecb01c00f0540a41f27c15007b4e1c207a
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-09-23 21:33:15 +0800

    services

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         <Hero />
         <Projects />
         <TechStack /> 
+        <Services />
         <Contact />
         <Footer />
         {/**<Services /> */}
