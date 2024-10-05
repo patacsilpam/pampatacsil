a272ce27b9bdf9d32f35b0e3a8ab958d18f57b30
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2024-10-05 16:15:27 +0800

    feat: Enhance page loading by removing unnecessary animations

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import { motion } from "framer-motion"
 
 const Hero = () => {
@@ -7,23 +6,27 @@ const Hero = () => {
     <div className="min-h-[450px] h-full">
       <div className="flex flex-row justify-center items-center my-44">
         <div className="flex flex-col justify-center items-center">
-          <div className="max-w-4xl w-full md:text-center px-[3rem] ">
+          <div className="max-w-3xl w-full md:text-center px-[3rem] ">
             {" "}
-            <TextGenerateEffect
-              words={`I'm Pam 👋, a software developer who turns ideas into interactive experience.`}
-              className="text-[#1d1e20] font-openSans font-semibold md:max-w-3xl w-full"
-            />
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.2, duration: 0.2 }} 
+                className="text-sm font-light text-[rgb(70,71,78)]">
+              <h1 className="text-[#1d1e20] text-xl md:text-5xl font-openSans font-semibold">
+                I'm Pam 👋, a software developer turns your ideas into interactive experience.
+              </h1>
+            </motion.div>
             {/* <div className="-mt-4  h-3 bg-gradient-to-r from-[#0525c8d6] to-[#041d995f] animate-expand" />*/}
             <div className="mt-5 transition ease-in-out  duration-75 delay-150">
               <motion.div  
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
-                  transition={{ delay: 1.5, duration: 1 }} 
-                  className="text-md font-light text-[#46474e]">
-                With a focus on user-centered design, I leverage my coding and
-                design expertise to create exceptional digital experiences.
-                Committed to delivering innovative solutions that engage and
-                delight users.
+                  transition={{ delay: 0.3, duration: 0.3 }}>
+                    <small className="text-[0.75rem] lg:text-xl font-light text-[#46474e]">With a focus on user-centered design, I leverage my coding and
+                    design expertise to create exceptional digital experiences.
+                    Committed to delivering innovative solutions that engage and
+                    delight users.</small>
               </motion.div>
             </div>
             
